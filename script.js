be1a63cbbf3abcb55efa919d6ed4d7bdc1f85c6c
Pedro Alencar <pedromariutti@gmail.com>
2023-02-22 19:28:13 -0300

    Add message on index page

--- script.js
+++ script.js
@@ -4,46 +4,53 @@ const veiculosCadastrados = JSON.parse(
 
 const sectionCardContainer = document.querySelector("section");
 
-for (const carro of veiculosCadastrados) {
-  console.log(carro);
-  const divCard = document.createElement("div");
-  divCard.className = "car-card";
-  const image = document.createElement("img");
-  image.src = carro.img;
-  image.className = "carImg";
+if (!veiculosCadastrados) {
+  const mensagem = document.createElement("h1");
+  mensagem.innerHTML =
+    "Não há veículos cadastrados. Inicie cadastrando marca, em seguida o modelo e depois o veículo.";
+  sectionCardContainer.appendChild(mensagem);
+} else {
+  for (const carro of veiculosCadastrados) {
+    console.log(carro);
+    const divCard = document.createElement("div");
+    divCard.className = "car-card";
+    const image = document.createElement("img");
+    image.src = carro.img;
+    image.className = "carImg";
 
-  const carDescriptionElem = document.createElement("h2");
-  carDescriptionElem.className = "carDescription";
-  carDescriptionElem.innerText = `${
-    carro.marca[0].toUpperCase() + carro.marca.slice(1)
-  } ${carro.modelo[0].toUpperCase() + carro.modelo.slice(1)}`;
+    const carDescriptionElem = document.createElement("h2");
+    carDescriptionElem.className = "carDescription";
+    carDescriptionElem.innerText = `${
+      carro.marca[0].toUpperCase() + carro.marca.slice(1)
+    } ${carro.modelo[0].toUpperCase() + carro.modelo.slice(1)}`;
 
-  const ulAtrib = document.createElement("ul");
-  ulAtrib.className = "ulCard";
+    const ulAtrib = document.createElement("ul");
+    ulAtrib.className = "ulCard";
 
-  const param = {
-    cor: carro.cor,
-    ano: carro.ano,
-    km: carro.km,
-    valor: carro.valor,
-  };
+    const param = {
+      cor: carro.cor,
+      ano: carro.ano,
+      km: carro.km,
+      valor: carro.valor,
+    };
 
-  for (const valor in param) {
-    const parametro = document.createElement("li");
-    parametro.className = "list-param";
-    parametro.innerText = `${valor}: ${param[valor]}`;
-    console.log(`${valor}: ${param[valor]}`);
-    ulAtrib.appendChild(parametro);
+    for (const valor in param) {
+      const parametro = document.createElement("li");
+      parametro.className = "list-param";
+      parametro.innerText = `${valor}: ${param[valor]}`;
+      console.log(`${valor}: ${param[valor]}`);
+      ulAtrib.appendChild(parametro);
+    }
+
+    const btnComprar = document.createElement("button");
+    btnComprar.innerHTML = "Comprar";
+    btnComprar.type = "button";
+    btnComprar.className = "btnComprar";
+
+    divCard.appendChild(image);
+    divCard.appendChild(carDescriptionElem);
+    divCard.appendChild(ulAtrib);
+    divCard.appendChild(btnComprar);
+    sectionCardContainer.appendChild(divCard);
   }
-
-  const btnComprar = document.createElement("button");
-  btnComprar.innerHTML = "Comprar";
-  btnComprar.type = "button";
-  btnComprar.className = "btnComprar";
-
-  divCard.appendChild(image);
-  divCard.appendChild(carDescriptionElem);
-  divCard.appendChild(ulAtrib);
-  divCard.appendChild(btnComprar);
-  sectionCardContainer.appendChild(divCard);
 }
